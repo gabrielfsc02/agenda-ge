4d5448141786b82151832383634492f02664c8b2
Gabriel Fernandes <48166186+gabrielfsc02@users.noreply.github.com>
2025-03-28 14:10:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,21 @@ async function carregarJogos() {
     return;
   }
 
-  const partes = dataInput.split('-');
+  // dataInput vem no formato yyyy-mm-dd; precisamos converter para dd-mm-yyyy
+  const partes = dataInput.split('-'); // [yyyy, mm, dd]
   const dataFormatada = `${partes[2]}-${partes[1]}-${partes[0]}`;
 
+  // URL do backend (substitua se necessário)
   const res = await fetch(`https://agenda-ge-backend.onrender.com/jogos/${dataFormatada}`);
   const dados = await res.json();
 
   const div = document.getElementById("resultados");
   div.innerHTML = '';
+
+  if (dados.erro) {
+    div.innerHTML = `<p style="color: red;">${dados.erro}</p>`;
+    return;
+  }
 
   Object.keys(dados).forEach(campeonato => {
     const h2 = document.createElement('h2');
